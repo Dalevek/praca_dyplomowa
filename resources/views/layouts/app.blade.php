53c9c27f6846c7fbd8cddcf731512f4895da8555
Chudy <p.chudalewski@gmail.com>
2018-05-23 17:40:26 +0200

    Update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <!-- Bootstrap core CSS -->
+        <!-- <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="{{asset('css/sticky-footer-navbar.css')}}" >
+
+        <title>{{$title}} - Projekt dyplomowy</title>
+
+    </head>
+    <body>
+        @include('inc.navbar')
+        <main role="main" class="container">
+            @yield('content')
+        </main>
+        @include('inc.footer')
+    </body>
+</html>
